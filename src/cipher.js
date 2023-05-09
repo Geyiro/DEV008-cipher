4dc6b7a406cb5d0bcf047f99ac26ec44ca081483
Erika Nagaki <enagaki.22@gmail.com>
2023-05-08 22:30:32 -0500

    First Working Version

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
+  encode: function (offset, text) {
+    let result = "";
+
+    for (let i = 0; i < text.length; i++) {
+      const asciiNumb = text[i].charCodeAt();
+
+      if (asciiNumb + offset >= 65 && asciiNumb + offset <= 90) {
+        result += String.fromCharCode(asciiNumb + offset);
+      }
+      else if (asciiNumb + offset > 90) {
+        result += String.fromCharCode((asciiNumb + offset - 65) % 26 + 65);
+      }
+    }
+    return result;
+  },
+  decode: function (offset, text) {
+    let result = "";
+
+    for (let i = 0; i < text.length; i++) {
+      const asciiNumb = text[i].charCodeAt();
+
+      if (asciiNumb - offset >= 65 && asciiNumb - offset <= 90) {
+        result += String.fromCharCode(asciiNumb - offset);
+      }
+      else if (asciiNumb - offset < 65) {
+        const paddingLeft = (asciiNumb - 65)
+        const movementsFromRight = (offset - paddingLeft - 1) % 26
+        result += String.fromCharCode(90 - movementsFromRight);
+      }
+    }
+    return result;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const input = document.getElementById('inputMessage');
+const offset = document.getElementById('offset');
+const encodeButton = document.getElementById("encodeButton");
+const textarea = document.getElementById('outputMessage');
+
+
+
+encodeButton.addEventListener("click", () => {
+    console.log({ textarea, input, offset })
+    const result = cipher.encode(parseInt(offset.value), input.value);
+    textarea.value = result;
+});
+
+
+//funcion para copiar outputtext
+const copyToClipboard = document.querySelector('#copyToClipboard');
+
+copyToClipboard.addEventListener('click', async function () {
+    await navigator.clipboard.writeText(textarea.value);
+    alert("textCopy");
+});
+
