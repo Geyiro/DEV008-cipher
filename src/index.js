46580770ba37013b8b06674e7aff7f19ed0ef8e5
Erika Nagaki <enagaki.22@gmail.com>
2023-05-10 09:08:35 -0500

    Fix ESLint errors

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const encodeButton = document.getElementById("encode-button");
 const encodedText = document.getElementById("output-text-encode");
 //to uppercase input
 inputForEncode.addEventListener("input", () => {
-    inputForEncode.value = inputForEncode.value.toUpperCase();
+  inputForEncode.value = inputForEncode.value.toUpperCase();
 });
 // calling encode function
 encodeButton.addEventListener("click", () => {
-    const result = cipher.encode(parseInt(encodeOffset.value), inputForEncode.value);
-    encodedText.value = result;
+  const result = cipher.encode(parseInt(encodeOffset.value), inputForEncode.value);
+  encodedText.value = result;
 });
 
 const inputForDecode = document.getElementById("input-text-decode");
@@ -21,19 +21,19 @@ const decodedText = document.getElementById("output-text-decode");
 
 //to uppercase input decode
 decodeButton.addEventListener("input", () => {
-    inputForDecode.addEventListener() = inputForDecode.value.toUpperCase();
+  inputForDecode.value = inputForDecode.value.toUpperCase();
 });
 // calling decode function
 decodeButton.addEventListener("click", () => {
-    const result = cipher.decode(parseInt(decodeOffset.value), inputForDecode.value);
-    decodedText.value = result;
+  const result = cipher.decode(parseInt(decodeOffset.value), inputForDecode.value);
+  decodedText.value = result;
 });
 
 //funcion para copiar outputtext de ventana encode
 const copyToClipboard = document.getElementById("copyToClipboard");
 
 copyToClipboard.addEventListener("click", async function () {
-    await navigator.clipboard.writeText(encodedText.value);
-    alert("textCopy");
+  await navigator.clipboard.writeText(encodedText.value);
+  alert("textCopy");
 });
 
